83decf97d179768e00cba1976b0a1d8bf866ce31
Damian Kolaska <d.kolaska@stud.elka.pw.edu.pl>
2020-11-04 16:09:34 +0100

    Display button printing Hello World on click using Pygame GUI

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello World")
+import sys
+import pygame
+import pygame_gui
+
+
+pygame.init()
+size = width, height = 800, 600
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+manager = pygame_gui.UIManager(size)
+
+hello_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((350, 275), (100, 50)),
+    text='Say Hello',
+    manager=manager
+)
+
+
+clock = pygame.time.Clock()
+
+while True:
+    time_delta = clock.tick(60) / 1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+        if event.type == pygame.USEREVENT:
+             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                 if event.ui_element == hello_button:
+                     print('Hello World!')
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    screen.fill(black)
+
+    manager.draw_ui(screen)
+    pygame.display.update()
