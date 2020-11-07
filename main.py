040c86b3798a80291302dee6b4e2af72ba15b0c5
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2020-11-07 03:01:59 +0100

    basic test for reversi

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from reversi import Reversi
-
 if __name__ == "__main__":
-    reversi = Reversi()
-    reversi.print_valid_moves()
-    print(reversi.num_of_valid_moves)
-    reversi.make_a_move(3, 2)
-    reversi.print_valid_moves()
-    print(reversi.num_of_valid_moves)
-    reversi.make_a_move(2, 2)
-    reversi.print_valid_moves()
-    print(reversi.num_of_valid_moves)
+    print("Somebody once told me")
+    print("The world is gonna roll me")
+    print("I ain't the sharpest tool in the shed")
