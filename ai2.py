d32f821b1e3e22f3b4bce8ff0422a84309789d8e
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2020-11-11 22:05:41 +0100

    ai2: alpha beta pruning, also some tests in main (needs some cleaning)

---
+++ ai2.py
@@ -0,0 +1,66 @@
+import copy
+
+from typing import Tuple
+
+from reversi import Reversi
+
+
+def get_optimal_move(game_state: Reversi, depth: int) -> Tuple[int, int]:
+    optimal_move = (-1, -1)
+
+    if depth < 1 or game_state.is_finished:
+        return optimal_move
+
+    is_maximizing = True if game_state.current_player == 1 else False
+    alpha = int(-pow(game_state.board_size, 2))
+    beta = int(pow(game_state.board_size, 2))
+
+    if is_maximizing:
+        max_val = -pow(game_state.board_size, 2)
+        for move in game_state.valid_moves_list:
+            val = minimax(game_state, move, depth - 1, alpha, beta)
+            if val > max_val:
+                max_val = val
+                optimal_move = move
+                alpha = val
+
+    else:
+        min_val = pow(game_state.board_size, 2)
+        for move in game_state.valid_moves_list:
+            val = minimax(game_state, move, depth - 1, alpha, beta)
+            if val < min_val:
+                min_val = val
+                optimal_move = move
+                beta = val
+
+    return optimal_move
+
+
+def minimax(game_state: Reversi, move: Tuple[int, int], depth: int, alpha: int, beta: int) -> int:
+    game = copy.deepcopy(game_state)
+    assert game.make_a_move(row=move[0], col=move[1])
+
+    if depth == 0 or game.is_finished:
+        return game.player1_points - game.player2_points
+
+    is_maximizing = True if game.current_player == 1 else False
+
+    if is_maximizing:
+        max_val = -pow(game.board_size, 2)
+        for move in game.valid_moves_list:
+            val = minimax(game, move, depth - 1, alpha, beta)
+            max_val = max(max_val, val)
+            alpha = max(alpha, val)
+            if beta <= alpha:
+                break
+        return max_val
+
+    else:
+        min_val = pow(game.board_size, 2)
+        for move in game.valid_moves_list:
+            val = minimax(game, move, depth - 1, alpha, beta)
+            min_val = min(min_val, val)
+            beta = min(beta, val)
+            if beta <= alpha:
+                break
+        return min_val
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from reversi import Reversi
 import ai
+import ai2
 
 if __name__ == "__main__":
     print("Somebody once told me")
@@ -23,17 +24,28 @@ if __name__ == "__main__":
     i = 0
     pl1score = []
     pl2score = []
+    noprunetimes = []
+    prunetimes = []
+    player = 3
+    comp = 4
     while not rev.is_finished:
         # input('next move')
-        t = time.process_time()
+        ttt = time.process_time()
         if rev.current_player == 1:
-            move = ai.get_optimal_move(rev, 3)
+            move = ai.get_optimal_move(rev, player)
+            moveprune = ai2.get_optimal_move(rev, player)
         else:
-            move = ai.get_optimal_move(rev, 5)
-        print(time.process_time() - t)
+            t = time.process_time()
+            move = ai.get_optimal_move(rev, comp)
+            noprunetimes.append(time.process_time() - t)
+            t = time.process_time()
+            moveprune = ai2.get_optimal_move(rev, comp)
+            prunetimes.append(time.process_time() - t)
+        print(time.process_time() - ttt)
         if move == (-1, -1):
             print("invalid")
             break
+        assert move == moveprune, f'{move}, {moveprune}'
         assert rev.make_a_move(move[0], move[1])
         pl1score.append(rev.player1_points)
         pl2score.append(rev.player2_points)
@@ -42,7 +54,11 @@ if __name__ == "__main__":
     print(rev.player1_points)
     print(rev.player2_points)
 
-    plt.plot(pl1score, color="black")
-    plt.plot(pl2score, color='red')
+    # plt.plot(pl1score, color="black")
+    # plt.plot(pl2score, color='red')
+    # plt.show()
+
+    plt.plot(noprunetimes, color="blue")
+    plt.plot(prunetimes, color='red')
     plt.show()
 
