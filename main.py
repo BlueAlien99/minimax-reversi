e086bf1d43d07e45c69fe7fedca95aa799876490
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2020-11-11 16:18:44 +0100

    minimax implemented (no ab pruning)

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import time
-import timeit
+
+from reversi import Reversi
+import ai
 
 if __name__ == "__main__":
     print("Somebody once told me")
     print("The world is gonna roll me")
     print("I ain't the sharpest tool in the shed")
 
-    gods = time.process_time()
+    # gods = time.process_time()
+    #
+    # xxx = 0
+    # to = input("how long?")
+    # for x in range(int(to)):
+    #     xxx += x
+    #
+    # print(time.process_time() - gods)
 
-    xxx = 0
-    to = input("how long?")
-    for x in range(int(to)):
-        xxx += x
+    rev = Reversi()
+    rev.print_board()
+    i = 0
+    while not rev.is_finished:
+        input('next move')
+        t = time.process_time()
+        move = ai.get_optimal_move(rev, 6)
+        print(time.process_time() - t)
+        if move == (-1, -1):
+            print("invalid")
+            break
+        assert rev.make_a_move(move[0], move[1])
+        rev.print_board()
 
-    print(time.process_time() - gods)
+
 
